c3dfc031ed7ccc2161e3a69abedeb89ad923f551
ibnulhayat <ibnulhayatkhan@gmail.com>
2021-04-25 03:00:51 +0600

    1592. Rearrange Spaces Between Words

--- EasyStringProblems/1592. Rearrange Spaces Between Words.cs
+++ EasyStringProblems/1592. Rearrange Spaces Between Words.cs
@@ -9,66 +9,74 @@ namespace EasyStringProblems
         Tags = string
         Problem no = 
     */
-    class RearrangeSpacesBetweenWords{
+    class RearrangeSpacesBetweenWords
+    {
 
-    List<string> wordList = new List<string>();
-    
-    public string ReorderSpaces(string text) {
-        
-        int totalSpaces = getTotalSpace(text);
-        int totalWords = wordList.Count;
-        
-        //Console.WriteLine(totalWords);
-        int equalSpace = totalWords == 1? 0: totalSpaces/(totalWords-1);
-        int endSpace = totalWords == 1 ? totalSpaces : totalSpaces % (totalWords - 1);
-        
-        //string space = " ".Repeat();
-        //Console.WriteLine(equalSpace);
-        
-        string newString = "";
-        for(int j=0; j<totalWords;j++){
-            newString += wordList[j];
-            int i = 0;
-            //Console.WriteLine(word != wordList[totalWords-1]);
-            if(j != totalWords-1 ){
-                newString += new String(' ',equalSpace);
-                
-            }
-        }
-        
-        if(endSpace > 0 )newString += new String(' ',endSpace);
-            
-            
-        
-        
-        
-        return newString;
-    }
-    
-    public int getTotalSpace(string s){
-        int totalSpace = 0;
-        string word = "";
-        
-        foreach(char ch in s){
-            if(ch != ' '){
-                word += ch;
-            }
-            if(ch == ' '){
-                totalSpace++;
-                if(word != ""){
-                    wordList.Add(word);
-                    word = "";
+        List<string> wordList = new List<string>();
+
+        public string ReorderSpaces(string text)
+        {
+
+            int totalSpaces = getTotalSpace(text);
+            int totalWords = wordList.Count;
+
+            //Console.WriteLine(totalWords);
+            int equalSpace = totalWords == 1 ? 0 : totalSpaces / (totalWords - 1);
+            int endSpace = totalWords == 1 ? totalSpaces : totalSpaces % (totalWords - 1);
+
+            //string space = " ".Repeat();
+            //Console.WriteLine(equalSpace);
+
+            string newString = "";
+            for (int j = 0; j < totalWords; j++)
+            {
+                newString += wordList[j];
+                //Console.WriteLine(word != wordList[totalWords-1]);
+                if (j != totalWords - 1)
+                {
+                    newString += new String(' ', equalSpace);
+
                 }
-                
             }
-        }
-        if(word != "") wordList.Add(word);
-        
-        
-        return totalSpace;
-    }
 
-        
+            if (endSpace > 0) newString += new String(' ', endSpace);
+
+
+
+
+
+            return newString;
+        }
+
+        public int getTotalSpace(string s)
+        {
+            int totalSpace = 0;
+            string word = "";
+
+            foreach (char ch in s)
+            {
+                if (ch != ' ')
+                {
+                    word += ch;
+                }
+                if (ch == ' ')
+                {
+                    totalSpace++;
+                    if (word != "")
+                    {
+                        wordList.Add(word);
+                        word = "";
+                    }
+
+                }
+            }
+            if (word != "") wordList.Add(word);
+
+
+            return totalSpace;
+        }
+
+
 
     }
 }
